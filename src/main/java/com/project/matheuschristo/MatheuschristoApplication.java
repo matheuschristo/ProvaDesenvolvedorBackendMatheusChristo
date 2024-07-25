da707a5084c8c6c8166af1c839c2fa7d9383142d
Matheus Christo <matheuschristo.09@gmail.com>
2024-07-25 20:16:18 -0300

    Initial commit

---
+++ src/main/java/com/project/matheuschristo/MatheuschristoApplication.java
@@ -0,0 +1,13 @@
+package com.project.matheuschristo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatheuschristoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatheuschristoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/matheuschristo/MatheuschristoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.matheuschristo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatheuschristoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
