cb8bc3a6a0419d7043312791e44d3dc47a11358e
Matheus Christo <matheuschristo.09@gmail.com>
2024-07-25 22:25:43 -0300

    add: ajustado controller

--- src/main/java/com/project/matheuschristo/controller/ProdutoServicoController.java
+++ src/main/java/com/project/matheuschristo/controller/ProdutoServicoController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/pedido")
+@RequestMapping("/api/produto_servico")
 public class ProdutoServicoController {
 }
